e6f562400f619e80a9f93cadb61b3762d7bbe558
Gurmeet499 <gurmeetbajaj4@gmail.com>
2026-02-05 12:50:43 +0500

    Create countPrime.cpp

---
+++ countPrime.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+int countPrimeNumber(int n){
+
+   vector<bool> isPrime(n+1,true);// track the value that are prime
+    int count =0;
+ 
+    for(int i=2;i<n;i++){
+        if(isPrime[i]){
+            count++;
+            for(int j =i*2;j<n; j = j+ i){
+                isPrime[j] =  false;
+            }
+        }
+
+    }
+    return count;
+
+}
+
+int main(){
+ int n =50;
+ 
+ cout<<countPrimeNumber(n)<<endl;
+
+}
