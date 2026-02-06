197c9d6d5f8888b62183b42e4738fc285bf5d718
Gurmeet499 <gurmeetbajaj4@gmail.com>
2026-02-06 23:54:42 +0500

    Update printDigits.cpp

--- printDigits.cpp
+++ printDigits.cpp
@@ -10,11 +10,33 @@ void printdigits(int n){
         n = n/10;
     }
 }
+void countdigits(int n){
+    int count =0;
+    while(n != 0){
+        int digit = n % 10;
+      count++;
+        n = n/10;
+    }
+    cout<<count<<endl;
 
+}
+
+void sumOfdigits(int n){
+    int sum =0;
+    while(n != 0){
+        int digit = n % 10;
+       sum += digit;
+        n = n/10;
+    }
+    cout<<sum<<endl;
+
+}
 
 int main(){
  int n = 3568;
 
  printdigits(n);
+ countdigits(n);
+ sumOfdigits(n);
  return 0;
 }
